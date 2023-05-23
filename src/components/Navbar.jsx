500b9208b1da56104f2b4a87bd0bd43b82091676
Miles Kerr <m.kerr.professional@gmail.com>
2023-05-23 16:51:49 -0500

    Navbar is done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,90 @@
-import React from 'react'
-import LogoBtn from './img/LogoBtn.png'
-const Navbar = () => {
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
-    const handleClick = e => {
-        console.log(e.target);
-        console.log("Logo Clicked")
-    }
+const Navbar = () => {
+    const navlinks = [
+        {
+            title: 'Candles',
+            link: '/candles',
+        },
+        {
+            title: 'Bawlers',
+            link: '/bawlers',
+        },
+        {
+            title: 'About',
+            link: '/about',
+        },
+        {
+            title: 'Login',
+            link: '/login',
+        },
+    ];
+
+    const [open, setOpen] = useState(false);
+
+    const handleMenu = () => {
+        setOpen((prev) => !prev);
+    };
 
     return (
-        <div>
-            <div className="grid grid-cols-12 text-7xl font-bold text-teal-600">
-                <div className="p-4 space-y-2 bg-gray-600 rounded shadow">
-                    <span className="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
-                    <span className="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
-                    <span className="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
-                </div>
-                <div className="bg-rose-800">
-                    <p>F</p>
-                </div>
-                <div className="bg-slate-900">
-                    <p>L</p>
-                </div>
-                <div className="bg-indigo-900">
-                    <p>O</p>
-                </div>
-                <div className="bg-blue-950">
-                    <p>W</p>
-                </div>
-                <div className="bg-teal-950">
-                    <p>E</p>
-                </div>
-                <div className="bg-lime-900">
-                    <p>R</p>
-                </div>
-                <div className="bg-yellow-700">
-                    <p>B</p>
-                </div>
-                <div className="bg-red-900">
-                    <p>U</p>
-                </div>
-                <div className="bg-violet-950">
-                    <p>D</p>
-                </div>
-                <div className="bg-fuchsia-950">
-                    <p>S</p>
-                </div>
-                <div>
-                    <div className="bg-orange-900">
-                        <button><img src={LogoBtn} alt='logobtn' onClick={handleClick} /></button>
+        <div className='bg-orange-900'>
+            <div className='mx-auto px-4 sm:px-6 lg:px-8'>
+                <div className='flex items-center justify-between h-16'>
+                    {/* left side of the navbar */}
+                    <div className='flex items-center'>
+                        <Link
+                            to='/'
+                            className='text-fuchsia-950 hover:bg-teal-800 px-3 py-2 rounded-md text-xl font-bold'
+                        >
+                            FlowerBuds
+                        </Link>
+                    </div>
+                    {/* right side of the navbar */}
+                    <div className='hidden md:block'>
+                        <div className='ml-10 flex items-baseline space-x-4'>
+                            {navlinks.map((link, index) => (
+                                <Link
+                                    to={link.link}
+                                    key={index}
+                                    className='text-fuchsia-950 hover:bg-teal-800 px-3 py-2 rounded-md text-xl font-bold transition-all duration-500'
+                                >
+                                    {link.title}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                    {/* Hamburger Button */}
+                    <div className='-mr-2 flex md:hidden'>
+                        <button
+                            type='button'
+                            onClick={handleMenu}
+                            className='inline-flex items-center justify-center p-2 rounded-md text-fuchsia-950 hover:bg-teal-800 transition-all duration-500 focus:outline-none focus:ring-2 focus:ring-offset-fuchsia-600 focus:ring-teal-600'
+                        >
+                            <span className='sr-only'>Open Main Menu</span>
+                            {open === true ? <FaTimes /> : <FaBars />}
+                        </button>
                     </div>
                 </div>
+                {open ? (
+                    <div className='md:hidden'>
+                        <div className='ox-2 pt-2 pb-3 space-y-1 sm:px-3'>
+                            {navlinks.map((link, index) => (
+                                <Link
+                                    to={link.link}
+                                    key={index}
+                                    className='text-fuchsia-950 hover:bg-teal-800 px-3 py-2 rounded-md text-xl font-bold transition-all duration-500 block'
+                                >
+                                    {link.title}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                ) : null}
             </div>
         </div>
+    );
+};
 
-    )
-}
-
-export default Navbar
+export default Navbar;
